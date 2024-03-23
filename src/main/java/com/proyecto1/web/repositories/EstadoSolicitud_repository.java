1347d58be932672ffe8486b0af701ec0289b2fdf
Luisa vargas <luchisvargasgomez@gmail.com>
2024-03-23 14:16:53 -0500

    se agregaron entidades

---
+++ src/main/java/com/proyecto1/web/repositories/EstadoSolicitud_repository.java
@@ -0,0 +1,9 @@
+package com.proyecto1.web.repositories;
+
+import org.springframework.data.repository.CrudRepository;
+
+import com.proyecto1.web.entities.EstadoSolicitud;
+
+public interface EstadoSolicitud_repository extends CrudRepository<EstadoSolicitud, Long>{
+
+}
